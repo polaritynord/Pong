f974c2278deab060d3f3ecce8a4ef90825117324
pyxledev0 <pyxledev0@gmail.com>
2022-07-21 12:35:03 +0300

    paddle movement & drawing

--- main.lua
+++ main.lua
@@ -6,13 +6,24 @@ local paddleRed
 
 function love.load()
     paddleBlue = paddle.new()
+    paddleBlue.color = "blue"
+    paddleBlue.position.x = 12
+    paddleBlue.position.y = 270
+
     paddleRed = paddle.new()
+    paddleRed.color = "red"
+    paddleRed.position.x = 946
+    paddleRed.position.y = 270
 end
 
 function love.update(delta)
-    
+    paddleBlue.update(delta)
+    paddleRed.update(delta)
 end
 
 function love.draw()
-
+    -- Draw sides
+    -- Draw paddles
+    paddleBlue.draw()
+    paddleRed.draw()
 end
